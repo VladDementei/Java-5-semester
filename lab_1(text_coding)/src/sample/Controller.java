2d742a08cf060c921392fe13f3d40a5ca450b63d
VladDementey <vladementei@gmail.com>
2019-09-04 10:26:08 +0200

    чтение и запись
    чтение по 1024 символа, изменена кодировка

--- lab_1(text_coding)/src/sample/Controller.java
+++ lab_1(text_coding)/src/sample/Controller.java
@@ -7,30 +7,46 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Controller {
 
     @FXML
     private TextArea inputArea;
-
     @FXML
     private MenuItem menuOpenFile;
 
+    private File file;
+
     public void openFile(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(new File("D:\\IDEA"));
-        File file = fileChooser.showOpenDialog(menuOpenFile.getParentPopup().getScene().getWindow());
+        fileChooser.setInitialDirectory(new File("\\IDEA\\3 курс"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        file = fileChooser.showOpenDialog(menuOpenFile.getParentPopup().getScene().getWindow());
         if (file != null) {
-            try {
-                Scanner sc = new Scanner(file);
-                inputArea.clear();
-                while (sc.hasNextLine()) {
-                    inputArea.appendText(sc.nextLine());
-                    inputArea.appendText("\n");
+            inputArea.clear();
+            char[] buf = new char[1024];
+            try(Reader reader = new InputStreamReader(new FileInputStream(file), "Cp1251")){
+                int flag;
+                while ((flag = reader.read(buf))>= 0){
+                    inputArea.appendText(String.valueOf(buf, 0, flag));
                 }
+            } catch (IOException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("File problems");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    public void saveFile(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File("\\IDEA\\3 курс"));
+        //fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        file = fileChooser.showSaveDialog(menuOpenFile.getParentPopup().getScene().getWindow());
+        if (file != null) {
+            try (Writer writer = new OutputStreamWriter(new FileOutputStream(file), "Cp1251")){
+                writer.write(inputArea.getText());
             } catch (IOException ex) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setHeaderText("File problems");
